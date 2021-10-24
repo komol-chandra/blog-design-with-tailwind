d868e83fed5b6e6ccd81e3c0fd5a30095d5523d9
komol-chandra <kiri2kaofficial@gmail.com>
2021-10-24 23:56:02 +0600

    header

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+				poppins: "'Poppins', sans-serif",
+				roboto: "'Roboto', sans-serif"
+			},
+			colors: {
+				primary: '#FD3D57',
+				secondary: '#1F2937',
+				background: '#E4EBFE'
+			}
+		}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
